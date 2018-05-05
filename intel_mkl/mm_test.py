f4885010ce38721fdf0aa62add0f965c31efc944
Alex Stein <astein.ny@gmail.com>
2018-05-05 22:53:01 +0000

    .

--- intel_mkl/mm_test.py
+++ intel_mkl/mm_test.py
@@ -1,11 +1,26 @@
 import tensorflow as tf
 import time
+from tensorflow.python.client import timeline
+
 tf.set_random_seed(42)
-A = tf.random_normal([30000,30000])
-B = tf.random_normal([30000,30000])
+A = tf.random_normal([10000,10000])
+B = tf.random_normal([10000,10000])
 def checkMM():
 	start_time = time.time()
 	with tf.Session() as sess:
-		print( sess.run( tf.reduce_sum( tf.matmul(A,B) ) ) )
+		# options to trace execution
+		options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+		run_metadata = tf.RunMetadata()
+
+		print( sess.run( tf.reduce_sum( tf.matmul(A,B) ),\
+				 options=options,\
+				 run_metadata=run_metadata ) )
+
+		# create timeline object and write to json
+		fetched_timeline = timeline.Timeline(run_metadata.step_stats)
+		chrome_trace = fetched_timeline.generate_chrome_trace_format()
+		with open('timeline_02.json', 'w') as f:
+			f.write(chrome_trace)
+
 	print(" took {} seconds".format(time.time() - start_time))
 checkMM()
