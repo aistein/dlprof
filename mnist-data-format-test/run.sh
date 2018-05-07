b5f45387006e99f45e76bbf19a2e3dc87083b738
mike alvarino <michael.alvarino@gmail.com>
2018-05-06 23:49:54 -0400

    remove unneccessary files

--- mnist-data-format-test/run.sh
+++
@@ -1,5 +0,0 @@
-python mnist_deep.py --data_format NCHW
-python mnist_deep.py --data_format NCHW
-python mnist_deep.py --data_format NCHW
-python mnist_deep.py --data_format NCHW
-python mnist_deep.py --data_format NCHW
